b1bd20641dc16e73f9019290cdf9a551fb6d13f5
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-21 09:46:15 -0300

    add script for checking outdated dependencies

---
+++ secure-docker-image/check-dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Executes npm audit and save the result (first line)
+audit_output=$(npm audit | head -n 1)
+
+expected="found 0 vulnerabilities"
+
+if [ "$audit_output" != "$expected" ]; then
+    echo "Error: vulnerabilities found"
+    exit 1
+fi
+
+outdated_output=$(npm outdated)
+
+if [ $(echo "$outdated_output" | wc -l) -gt 1 ]; then
+    echo "Error: Se encontraron paquetes desactualizados."
+    echo "$outdated_output"
+    exit 1
+fi
+
+echo "Dependencies OK"
