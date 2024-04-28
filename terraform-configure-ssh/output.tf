f45cb017e52b9b26bba3b8e7f3d2cdb5d69b47a7
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-28 10:31:23 -0300

    testing pre-commit hooks

--- terraform-configure-ssh/output.tf
+++ terraform-configure-ssh/output.tf
@@ -1,6 +1,6 @@
 output "instance_public_ips" {
   value       = [for instance in google_compute_instance.vm_instance : "${instance.name} - ${instance.network_interface[0].access_config[0].nat_ip}"]
-  description = "The public IP addresses of the VM instances."
+  description = "The public IP addresses of the VM instances"
 }
 
 output "instance_ip" {
